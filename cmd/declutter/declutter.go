5aacfc8461b5a429ed53d5f71ee2f9efc2d99f9e
Eddie023 <manish23chaulagain@gmail.com>
2021-05-12 18:10:04 +0545

    Add function to move file

--- cmd/declutter/declutter.go
+++ cmd/declutter/declutter.go
@@ -5,14 +5,18 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/eddie023/declutter/internal"
 )
 
-func MoveFiles(cmd string) int {
-	dir := ReadDir(cmd)
+type outputFoldersMap map[string][]string
 
+func MoveFiles(cmd string) int {
+	outputFolders := getOutputFolders()
+
+	dir := ReadDir(cmd)
 	// Function to filter strings with "." at the beginning. i.e hidden files
 	ss := func(fName string) bool { return !strings.HasPrefix(string(fName), ".") }
 
@@ -30,7 +34,24 @@ func MoveFiles(cmd string) int {
 		}
 
 		// Move files to folders specified in config file
-		// fmt.Println(fName, "and", fType)
+		if shouldMoveFile(fType, outputFolders) {
+			outputFolderFilePath, ok := mapkey(outputFolders, fType)
+			if !ok {
+				panic("Output type doesn't exist")
+			}
+
+			oldpath, _ := filepath.Abs(fName)
+			newpath, err := filepath.Abs(outputFolderFilePath)
+			if err != nil {
+				log.Fatal("Can't create new path", err)
+			}
+			newpath = newpath + "/" + fName
+
+			err = MoveFile(oldpath, newpath)
+			if err != nil {
+				log.Fatal("Error when moving files", err)
+			}
+		}
 	}
 
 	return len(filteredDir)
@@ -44,10 +65,69 @@ func ReadDir(path string) []os.FileInfo {
 	}
 
 	if len(dir) == 0 {
-		log.Print("Empty Directory")
+		log.Print("Empty Directory\n")
 
 		return []os.FileInfo{}
 	}
 
 	return dir
+}
+
+func getOutputFolders() outputFoldersMap {
+	var c internal.Conf
+	config := c.GetConf()
+	outputFolder := config.Output
+
+	for folder := range outputFolder {
+		if _, err := os.Stat(folder); os.IsNotExist(err) {
+			fmt.Printf("Folder name: %s doesn't exit\n", folder)
+
+			err := os.Mkdir(folder, 0755)
+			if err != nil {
+				log.Fatal("Error when creating new folder\n", err)
+			}
+		} else {
+			// If folder exist
+			fmt.Printf("Folder name: %s already exists\n", folder)
+		}
+	}
+
+	return outputFolder
+}
+
+func shouldMoveFile(t string, o outputFoldersMap) bool {
+	for _, v := range o {
+		for _, fileType := range v {
+			if fileType == t {
+				fmt.Println("File type matched", fileType, t)
+
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func MoveFile(oldpath string, newpath string) error {
+	return os.Rename(oldpath, newpath)
+}
+
+func mapkey(m map[string][]string, value string) (key string, ok bool) {
+	for k, v := range m {
+		if contains(v, value) {
+			key = k
+			ok = true
+			return
+		}
+	}
+	return
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/eddie023/declutter/cmd/declutter"
-	"github.com/eddie023/declutter/internal"
 )
 
 const CURRENT_DIR = "."
 
 func main() {
-	var c internal.Conf
-	fmt.Println("the config is", c.GetConf())
-
 	cmd := ""
 	if len(os.Args) > 1 {
 		cmd = os.Args[1]
