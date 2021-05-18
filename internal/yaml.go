8b110225babb964698c956acbf84e7133811726a
Eddie023 <manish23chaulagain@gmail.com>
2021-05-18 08:35:50 +0545

    Update GetConf method to exit when error

--- internal/yaml.go
+++ internal/yaml.go
@@ -1,16 +1,13 @@
 package internal
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"path/filepath"
 
 	"gopkg.in/yaml.v2"
 )
-
-// FIXME: Fix absolute path problem
-var configFilePATH, _ = filepath.Abs("internal/../config.yaml")
 
 // This makes output key-value in yaml generic
 type Conf struct {
@@ -18,15 +15,20 @@ type Conf struct {
 }
 
 func (c *Conf) GetConf() *Conf {
-	fmt.Println(configFilePATH)
+	configFilePATH, _ := filepath.Abs("./config.yaml")
+
 	yamlFile, err := ioutil.ReadFile(configFilePATH)
 	if err != nil {
-		log.Printf("yamlFile.Get arr #%v ", err)
+		log.Printf("Error: YamlFile.Get arr #%v ", err)
+
+		os.Exit(1)
 	}
 
 	err = yaml.Unmarshal(yamlFile, c)
 	if err != nil {
-		log.Fatalf("Unmarshal: %v", err)
+		log.Fatalf("Error: Unmarshal Failed: %v", err)
+
+		os.Exit(1)
 	}
 
 	return c
