0b09e2dc42ceaca4a64e4960417e08b61a7fbee8
Eddie023 <manish23chaulagain@gmail.com>
2021-05-18 08:36:14 +0545

    Create separate file for path functions

--- internal/filter.go
+++ internal/filter.go
@@ -1,9 +1,7 @@
 package internal
 
 import (
-	"log"
 	"os"
-	"path/filepath"
 )
 
 func FilterByName(ss []os.FileInfo, test func(string) bool) (ret []os.FileInfo) {
@@ -22,23 +20,4 @@ func FilterByFileInfo(ss []os.FileInfo, test func(os.FileInfo) bool) (ret []os.F
 		}
 	}
 	return ret
-}
-
-func GetDirPath(cmd string) (path string) {
-	if cmd == "." {
-		path = getAbsPath()
-	} else {
-		path = cmd
-	}
-	return path
-}
-
-func getAbsPath() string {
-	ex, err := os.Executable()
-	if err != nil {
-		log.Fatal(err)
-	}
-	path := filepath.Dir(ex)
-
-	return path
 }
